a839b3423c8c5446606f98096430d0596ad79cda
Vladislav Alekseevskiy <86176957+Taumaturgist@users.noreply.github.com>
2023-04-29 19:40:13 +0700

    Add FPS info on UI

--- Assets/!Scripts/CarDashboard.cs
+++ Assets/!Scripts/CarDashboard.cs
@@ -23,5 +23,11 @@ public class CarDashboard : MonoBehaviour
         carCurrentRideDistance.text = $"Current Ride Distance, m: {_activeCarInfo.GetCurrentRideDistance()}";
         carOdometer.text = $"Odometer, m: {_playerAccount.GetOdometer()}";
         carNitroCapacity.text = $"NITRO: {_activeCarInfo?.GetNitroCapacity()}";
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), Mathf.RoundToInt(1.0f / Time.smoothDeltaTime).ToString());
     }
 }
