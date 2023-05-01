ad235eb3d77ae29e009ea41f32c547c3de09191f
Buyan Lyundup <blyundup@gmail.com>
2023-05-01 22:13:04 +0700

    TemplateUpdate

--- Assets/Configs/BlockSpawnConfig.cs
+++ Assets/Configs/BlockSpawnConfig.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class BlockSpawnConfig : ScriptableObject
 {
     [Header("Blocks")]
-    public GameObject BlockStorage;
-    public GameObject Block;
+    public BlockSpawner BlockSpawner;
+    public Block Block;
 
     public float OffsetZ;
 
