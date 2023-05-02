7dcb638184832ea62f5d255f39c31414680d2c39
Vladislav Alekseevskiy <86176957+Taumaturgist@users.noreply.github.com>
2023-05-02 14:32:53 +0700

    Expand BuildingSpawner (not finished)

--- Assets/!Scripts/Buildings/BuildingSpawner.cs
+++ Assets/!Scripts/Buildings/BuildingSpawner.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BuildingSpawner : MonoBehaviour
 {
     [SerializeField] private Vector3[] buildingSpawnPoints;
     [SerializeField] private BuildingSpawnConfig buildingSpawnConfig; //set as private and put config to AppStartUp
     [SerializeField] private Building buildingPrefab;
+
+    [SerializeField] private int[] smallBuildingIndexes;
+
+    private void Awake()
+    {
+        for (int i = 0; i < buildingSpawnPoints.Length; i++)
+        {
+            Instantiate(buildingPrefab, transform.position, Quaternion.identity);
+        }
+    }
 
     private GameObject[] _bigFirstFloors;
     private GameObject[] _smallFirstFloors;
