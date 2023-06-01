affcc80c3ae5524c95315135aaecb859975554ba
blyundup <blyundup@gmail.com>
2023-06-01 21:32:52 +0700

    middle and roof create

--- Assets/!Scripts/Buildings/Building.cs
+++ Assets/!Scripts/Buildings/Building.cs
@@ -3,24 +3,49 @@ using System.Collections.Generic;
 
 public class Building : MonoBehaviour
 {
+    private BuildingSpawnConfig _buildingSpawnConfig;
     private List<GameObject> _bigFirstFloorsList;
     private List<GameObject> _smallFirstFloorsList;
     private List<int> _smallBuildingsIndexesList;
 
+    private List<GameObject> _bigMiddleFloorsList;
+    private List<GameObject> _smallMiddleFloorsList;
+
+    private List<GameObject> _bigRoofList;
+    private List<GameObject> _smallRoofList;
+
     public delegate void Delegate();
 
+    private Vector3 _offset;
     private int _buildingNumber;
     private bool _isOnLeftStreetSide;
     private float _angleY;
+    private float _height = 5f;
+
+    private int _randomIndex;
+    private int _minFloorAmount;
+    private int _maxFloorAmount;
+
+    private bool _isBigBuilding;
+
 
     public void Launch(BuildingSpawnConfig buildingSpawnConfig, List<int> smallBuildingsIndexes, int buildingNumber, bool isOnLeftStreetSide)
     {
+        _buildingSpawnConfig = buildingSpawnConfig;
         _smallBuildingsIndexesList = smallBuildingsIndexes;
         _buildingNumber = buildingNumber;
         _isOnLeftStreetSide = isOnLeftStreetSide;
+        _minFloorAmount = _buildingSpawnConfig.MinFloorsAmount;
+        _maxFloorAmount = _buildingSpawnConfig.MaxFloorsAmount;
 
-        _bigFirstFloorsList = buildingSpawnConfig.BigFirstFloorsList;
-        _smallFirstFloorsList = buildingSpawnConfig.SmallFirstFloorsList;
+        _bigFirstFloorsList = _buildingSpawnConfig.BigFirstFloorsList;
+        _smallFirstFloorsList = _buildingSpawnConfig.SmallFirstFloorsList;
+
+        _bigMiddleFloorsList = _buildingSpawnConfig.BigMiddleFloorsList;
+        _smallMiddleFloorsList = _buildingSpawnConfig.SmallMiddleFloorsList;
+
+        _bigRoofList = _buildingSpawnConfig.BigRoofList;
+        _smallRoofList = _buildingSpawnConfig.SmallRoofList;
 
         CreateBuilding();
     }
@@ -28,8 +53,9 @@ public class Building : MonoBehaviour
     private void CreateBuilding()
     {
         CreateFirstFloor();
-        //CreateMiddleFloors();
-        //CreateRoof();
+        CreateMiddleFloors(_isBigBuilding);
+        CreateRoof(_isBigBuilding);
+
         SetTransform();
     }
     private void CreateFirstFloor()
@@ -41,14 +67,45 @@ public class Building : MonoBehaviour
     }
     private void CreateSmallFirstFloor()
     {
+        _isBigBuilding = false;
+
         var randomInt = Random.Range(0, _smallFirstFloorsList.Count);
         Instantiate(_smallFirstFloorsList[randomInt], transform.position, transform.rotation, transform);
+        _randomIndex = randomInt;
     }
     private void CreateBigFirstFloor()
     {
+        _isBigBuilding = true;
+
         var randomInt = Random.Range(0, _bigFirstFloorsList.Count);
         Instantiate(_bigFirstFloorsList[randomInt], transform.position, transform.rotation, transform);
+        _randomIndex = randomInt;
     }
+
+    private void CreateMiddleFloors(bool isBigBuilding)
+    {
+        var randomFloorAmount = Random.Range(_minFloorAmount, _maxFloorAmount + 1);
+        
+
+        for (int i = 0; i < randomFloorAmount; i++)
+        {
+            _offset = new Vector3(0f, (i + 1) * _height, 0f);
+            if (isBigBuilding)
+                Instantiate(_bigMiddleFloorsList[_randomIndex], transform.position + _offset, transform.rotation, transform);
+            else
+                Instantiate(_smallMiddleFloorsList[_randomIndex], transform.position + _offset, transform.rotation, transform);
+        }
+        _offset += new Vector3(0f, _height, 0f);
+    }
+
+    private void CreateRoof(bool isBigBuilding)
+    {
+        if (isBigBuilding)
+            Instantiate(_bigRoofList[_randomIndex], transform.position + _offset, transform.rotation, transform);
+        else
+            Instantiate(_smallRoofList[_randomIndex], transform.position + _offset, transform.rotation, transform);
+    }
+
     private void SetTransform()
     {
         Delegate actionBasedOnSmallBuildingsIndexesCount = ActionBasedOnSmallBuildingsIndexesCount;
--- Assets/Configs/ConfigScripts/BuildingSpawnConfig.cs
+++ Assets/Configs/ConfigScripts/BuildingSpawnConfig.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "BuildingSpawnConfig", menuName = "Configs/BuildingSpawnConfig", order = 51)]
 public class BuildingSpawnConfig : ScriptableObject
 {
+    public int MinFloorsAmount;
+    public int MaxFloorsAmount;
+
     public List<GameObject> BigFirstFloorsList;
     public List<GameObject> SmallFirstFloorsList;
 
-    public int MinFloorsAmount;
-    public int MaxFloorsAmount;
+    public List<GameObject> BigMiddleFloorsList;
+    public List<GameObject> SmallMiddleFloorsList;
+
+    public List<GameObject> BigRoofList;
+    public List<GameObject> SmallRoofList;
 }
