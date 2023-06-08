54a912789004c153a0238bcc98c6e030962ce3f1
Vladislav Alekseevskiy <86176957+Taumaturgist@users.noreply.github.com>
2023-06-08 09:42:23 +0700

    Implement UIMainScreen and UIHUD

--- Assets/!Scripts/UI/UIMainMenu.cs
+++ Assets/!Scripts/UI/UIMainMenu.cs
@@ -5,17 +5,5 @@ using TMPro;
 
 public class UIMainMenu : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI highScoreText;
-
-    private float _highScore;
-
-    private void Awake()
-    {
-        //get Odometer from CarDashBoard
-        //_highScore = Odometer;
-    }
-    private void Start()
-    {
-        highScoreText.text = $"HighScore: {_highScore}";
-    }
+    
 }
