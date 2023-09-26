15c04b725044579b5bd4b9e2aded452515c3ca8e
Lost_Shadow <sobolm58@gmail.com>
2023-09-26 20:26:10 +0700

    traffic car driving

--- Assets/!Scripts/Traffic/TrafficCar/TrafficCarMovement.cs
+++ Assets/!Scripts/Traffic/TrafficCar/TrafficCarMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace Traffic
@@ -7,26 +8,63 @@ namespace Traffic
     {
         [HideInInspector] public TrafficCarState State;
         private float _speed;
+        private float _slowSpeed;
+        private float _rayDistance = 2.0f;
+        private Rigidbody _rigidbody;
+        private Vector3 _direction;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Start()
         {
             _speed = Random.Range(5.0f, 5.5f);
-            State = TrafficCarState.Rides;
+            State = TrafficCarState.Driving;
+            _direction = transform.forward;
         }
 
         private void Update()
         {
-            if (State == TrafficCarState.Rides)
+            RealseRays();
+            
+            switch (State)
             {
-                if (transform.position.x < 0)
-                {
-                    transform.Translate(-transform.TransformDirection(Vector3.forward) * _speed * Time.deltaTime);
-                }
-                else
-                {
-                    transform.Translate(transform.TransformDirection(Vector3.forward) * _speed * Time.deltaTime);
-                }
+                case TrafficCarState.Driving:
+                    Drive();
+                    return;
+                case TrafficCarState.SlowDown:
+                    Stop();
+                    return;
+                case TrafficCarState.Stopped:
+                    Stop();
+                    return;
             }
+        }
+
+        private void RealseRays()
+        {
+            RaycastHit hit;
+            
+        }
+        
+        private void Drive()
+        {
+            _rigidbody.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
+        }
+        
+        private void SlowDown()
+        {
+            _rigidbody.MovePosition(transform.position + 
+                                    transform.forward *
+                                    Mathf.MoveTowards(_speed, _slowSpeed, Time.deltaTime));
+        }
+        private void Stop()
+        {
+            _rigidbody.MovePosition(transform.position +
+                                    transform.forward *
+                                    Mathf.MoveTowards(_speed, 0f, Time.deltaTime));
         }
     }
 }
