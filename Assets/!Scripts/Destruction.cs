a531742528e0a71488f268ac58bde18b98f332b7
Lyundup_Buyan <blyundup@gmail.com>
2023-09-28 18:58:52 +0700

    add props destruction

--- Assets/!Scripts/Destruction.cs
+++ Assets/!Scripts/Destruction.cs
@@ -1,37 +1,41 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destruction : MonoBehaviour
 {
-    private List<Transform> _childsTransforms = new();
-    private Transform _parentTransform;
-    private float _force = 10f;
+    private readonly List<Transform> _childsTransforms = new();
+    private readonly float _force = 5f;
+    private bool _firstCollition = true;
 
     private void Start()
     {
-        _parentTransform = transform.GetChild(0);
-
-        for (var i = 0; i < _parentTransform.childCount; i++)
+        for (var i = 0; i < transform.childCount; i++)
         {
-            _childsTransforms.Add(_parentTransform.GetChild(i));
+            _childsTransforms.Add(transform.GetChild(i));
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.transform.CompareTag("Player"))
+        if (other.transform.CompareTag("Player") && _firstCollition)
         {
-            DestroyObject();
+            _firstCollition = false;
+            StartCoroutine("DestroyObject");
         }
     }
 
-    private void DestroyObject()
+    private IEnumerator DestroyObject()
     {
         foreach (var childTransform in _childsTransforms)
         {
-            var rb = childTransform.GetComponent<Rigidbody>();
+            var rb = childTransform.gameObject.AddComponent<Rigidbody>();
             rb.useGravity = true;
             rb.AddForce(Random.onUnitSphere * _force);
         }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
     }
 }
